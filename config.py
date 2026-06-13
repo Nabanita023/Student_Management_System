7f621a37c1fcce7a337427883c2d0e61aa6a92df
Nabanita023 <snabanita987@gmail.com>
2026-06-14 02:43:12 +0530

    main

--- config.py
+++ config.py
@@ -1,10 +1,20 @@
-import os
+# import os
 
+# class Config:
+#     MYSQL_HOST = os.environ.get('MYSQL_HOST', 'thomas.proxy.rlwy.net')
+#     MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
+#     MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'naba@123')
+#     MYSQL_DATABASE = os.environ.get('MYSQL_DATABASE', 'railway')
+#     MYSQL_PORT = int(os.environ.get('MYSQL_PORT', 3306))
+#     SECRET_KEY = os.environ.get('SECRET_KEY', 'abc@nfkhhokokjmjhgbiugiioj9ypujidj98uipuju')
+#     DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+import os
 class Config:
-    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'thomas.proxy.rlwy.net')
+    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'localhost')
     MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '')
     MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'naba@123')
-    MYSQL_DATABASE = os.environ.get('MYSQL_DATABASE', 'railway')
-    MYSQL_PORT = int(os.environ.get('MYSQL_PORT', 3306))
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'abc@nfkhhokokjmjhgbiugiioj9ypujidj98uipuju')
+    MYSQL_DATABASE = os.environ.get('MYSQL_DATABASE', 'student_management')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'naba@kfvjihhoiojdkhyudgihhgygy')
     DEBUG = os.environ.get('DEBUG', 'False') == 'True'

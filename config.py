21932de32b7b0c2e79bd870d671be21c60f9b2fd
Nabanita023 <snabanita987@gmail.com>
2026-06-14 02:03:59 +0530

    main

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ def get_db_connection():
         host=os.environ.get('MYSQL_HOST', 'localhost'),
         user=os.environ.get('MYSQL_USER', 'root'),
         password=os.environ.get('MYSQL_PASSWORD', 'naba@123'),
-        database=os.environ.get('MYSQL_DATABASE', 'student_management'),
+        database=os.environ.get('MYSQL_DATABASE', 'railway'),
         port=int(os.environ.get('MYSQL_PORT', 5000))
     )
     return conn
